81c274683e7de47d7bc94209f38d3af1dbaf332a
Igor Bulakh <igorbulakh1987@yandex.ru>
2020-04-24 09:45:31 +0300

    Added .travis.yml

--- main.go
+++ main.go
@@ -16,11 +16,17 @@ import (
 	"github.com/Luxurioust/excelize"
 )
 
+/*
+Data Структура Data
+*/
 type Data struct {
 	Name  string `xml:"name,attr"`
 	Value string `xml:"value"`
 }
 
+/*
+Placemark Структура Placemark
+*/
 type Placemark struct {
 	XMLName      xml.Name `xml:"Placemark"`
 	Name         string   `xml:"name"`
@@ -28,39 +34,57 @@ type Placemark struct {
 	ExtendedData []Data   `xml:"ExtendedData>Data"`
 }
 
+/*
+Folder Структура Folder
+*/
 type Folder struct {
 	XMLName    xml.Name    `xml:"Folder"`
 	Name       string      `xml:"name"`
 	Placemarks []Placemark `xml:"Placemark"`
 }
 
+/*
+Document Структура Document
+*/
 type Document struct {
 	XMLName xml.Name `xml:"Document"`
 	Folders []Folder `xml:"Folder"`
 }
 
+/*
+KML Структура kml файла
+*/
 type KML struct {
 	XMLName  xml.Name `xml:"kml"`
 	Document Document `xml:"Document"`
 }
 
+/*
+Person Структура мерчендайзера
+*/
 type Person struct {
 	Name     string
 	Address  string
 	Lat      float64
 	Long     float64
-	Selected []Store
+	Selected []Store // TODO: Сделать список доступных ТТ уникальным
 }
 
+/*
+Store Структура магазина
+*/
 type Store struct {
 	Name     string
 	Address  string
 	Long     float64
 	Lat      float64
 	Comments string
-	Selected []Person
+	Selected []Person // TODO: Сделать список доступных мерчей уникальным
 }
 
+/*
+KmlParser Структура парсера
+*/
 type KmlParser struct {
 	File    string
 	Name    string
@@ -72,6 +96,9 @@ type KmlParser struct {
 	Stores  []Store
 }
 
+/*
+StartParse Начало парсинга
+*/
 func (p KmlParser) StartParse(file string) KmlParser {
 	xmlFile, err := os.Open(file)
 
@@ -79,7 +106,7 @@ func (p KmlParser) StartParse(file string) KmlParser {
 		fmt.Println(err)
 	}
 
-	fmt.Println("Successfully opened open_test2.kml")
+	fmt.Println("Successfuly opened file " + strings.Split(file, "/")[len(strings.Split(file, "/"))-1])
 
 	defer xmlFile.Close()
 
@@ -88,7 +115,7 @@ func (p KmlParser) StartParse(file string) KmlParser {
 	var document KML
 
 	xml.Unmarshal(byteValue, &document)
-
+	fmt.Println("Parsing in progress...")
 	for i := 0; i < len(document.Document.Folders); i++ {
 		p.Folders = append(p.Folders, document.Document.Folders[i])
 	}
@@ -96,16 +123,43 @@ func (p KmlParser) StartParse(file string) KmlParser {
 	return p
 }
 
+func uniquePerson(intSlice []Person) []Person {
+	keys := make(map[*Person]bool)
+	list := []Person{}
+	for _, entry := range intSlice {
+		if _, value := keys[&entry]; !value {
+			keys[&entry] = true
+			list = append(list, entry)
+		}
+	}
+	return list
+}
+
+func uniqueStore(intSlice []Store) []Store {
+	keys := make(map[*Store]bool)
+	list := []Store{}
+	for _, entry := range intSlice {
+		if _, value := keys[&entry]; !value {
+			keys[&entry] = true
+			list = append(list, entry)
+		}
+	}
+	return list
+}
+
+/*
+GetCoordinatesByAddress Получение координат по адресу
+*/
 func GetCoordinatesByAddress(address string) []string {
 	uri := "https://geocode-maps.yandex.ru/1.x"
-	yandex_api := "ea85fc44-fa6f-41db-8dd6-f968590a02fc"
+	yandexAPI := "ea85fc44-fa6f-41db-8dd6-f968590a02fc"
 	req, err := url.Parse(uri)
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	params := url.Values{}
-	params.Add("apikey", yandex_api)
+	params.Add("apikey", yandexAPI)
 	params.Add("format", "json")
 	params.Add("geocode", address)
 	req.RawQuery = params.Encode()
@@ -139,6 +193,9 @@ func GetCoordinatesByAddress(address string) []string {
 	return coordinates
 }
 
+/*
+GetType Получить тип объекта
+*/
 func GetType(pl Placemark) string {
 	var ttype string
 	for i := 0; i < len(pl.ExtendedData); i++ {
@@ -151,15 +208,26 @@ func GetType(pl Placemark) string {
 	return ttype
 }
 
+/*
+ParseFolder Парсинг блока ТТ или блока с мерчендайзерами
+*/
 func (p KmlParser) ParseFolder() KmlParser {
 	for i := 0; i < len(p.Folders); i++ {
 		if p.Folders[i].XMLName.Local == "Folder" {
 			for j := 0; j < len(p.Folders[i].Placemarks); j++ {
 				ttype := GetType(p.Folders[i].Placemarks[j])
 				if ttype == "person" {
-					p.Persons = append(p.Persons, p.ParsePerson(p.Folders[i].Placemarks[j]))
+					person := p.ParsePerson(p.Folders[i].Placemarks[j])
+					if person.Name != "" {
+						p.Persons = append(p.Persons, person)
+					}
+					// TODO: Добавить определение города мерча
 				} else if ttype == "store" {
-					p.Stores = append(p.Stores, p.ParseStore(p.Folders[i].Placemarks[j]))
+					store := p.ParseStore(p.Folders[i].Placemarks[j])
+					if store.Name != "" {
+						p.Stores = append(p.Stores, store)
+					}
+					// TODO: Добавить определение города магазина
 				}
 			}
 		}
@@ -168,6 +236,9 @@ func (p KmlParser) ParseFolder() KmlParser {
 	return p
 }
 
+/*
+ParsePerson Парсинг типа Person и создание объекта
+*/
 func (p KmlParser) ParsePerson(pm Placemark) Person {
 	var Name string
 	Coordinates := make(map[string]float64)
@@ -194,7 +265,11 @@ func (p KmlParser) ParsePerson(pm Placemark) Person {
 	return Person{}
 }
 
+/*
+ParseStore Парсинг типа Store и создание объекта
+*/
 func (p KmlParser) ParseStore(pm Placemark) Store {
+	// TODO: Добавить проверку отсутсвия адреса и добавление адреса из блока ExtentionData
 	com := ""
 	Coordinates := make(map[string]float64)
 	for i := 0; i < len(pm.ExtendedData); i++ {
@@ -226,37 +301,56 @@ func (p KmlParser) ParseStore(pm Placemark) Store {
 	return Store{}
 }
 
+/*
+CreateDataMap Подготовка данных, рассчет расстояния
+*/
 func CreateDataMap(stores []Store, persons []Person, limit int) {
-	var s_name []string
-	var s_address []string
-	var s_comment []string
+	if limit == 0 {
+		limit = 1000
+	}
+	var sName []string
+	var sAddress []string
+	var sComment []string
 	var long []float64
 	var lat []float64
-	var p_name []string
-	var p_address []string
+	var pName []string
+	var pAddress []string
 	for i := 0; i < len(stores); i++ {
 		for j := 0; j < len(persons); j++ {
 			distance := GetDistance(stores[i].Lat, stores[i].Long, persons[j].Long, persons[j].Lat, "M")
+			// TODO: Добавить возможность задавать дистанцию от и до
 			if distance <= limit {
 				stores[i].Selected = append(stores[i].Selected, persons[j])
 				persons[j].Selected = append(persons[j].Selected, stores[i])
-
+				// TODO: Добавить обработку магазинов, у которых нет доступных мерчей поблизости
 				for p := 0; p < len(stores[i].Selected); p++ {
-					s_name = append(s_name, stores[i].Name)
-					s_address = append(s_address, stores[i].Address)
-					s_comment = append(s_comment, stores[i].Comments)
+					sName = append(sName, stores[i].Name)
+					sAddress = append(sAddress, stores[i].Address)
+					sComment = append(sComment, stores[i].Comments)
 					long = append(long, stores[i].Long)
 					lat = append(lat, stores[i].Lat)
-					p_name = append(p_name, stores[i].Selected[p].Name)
-					p_address = append(p_address, stores[i].Selected[p].Address)
+					pName = append(pName, stores[i].Selected[p].Name)
+					pAddress = append(pAddress, stores[i].Selected[p].Address)
 				}
 			}
 		}
 	}
+	// fmt.Println(len(stores))
+	// for _, k := range stores {
+	// 	fmt.Println("====", k, "\n")
+	// }
+	// uniqueStore(stores)
+	// fmt.Println(len(stores))
+	// for _, k := range stores {
+	// 	fmt.Println("====", k, "\n")
+	// }
 
-	CreateExcelFile(s_name, s_address, s_comment, long, lat, p_name, p_address)
+	CreateExcelFile(sName, sAddress, sComment, long, lat, pName, pAddress)
 }
 
+/*
+GetDistance Расчет дистанции между объектами
+*/
 func GetDistance(lat1 float64, lng1 float64, lat2 float64, lng2 float64, unit ...string) int {
 	const PI float64 = 3.141592653589793
 
@@ -289,9 +383,12 @@ func GetDistance(lat1 float64, lng1 float64, lat2 float64, lng2 float64, unit ..
 	return int(dist)
 }
 
-func CreateExcelFile(s_name []string, s_address []string,
-	s_comment []string, long []float64,
-	lat []float64, p_name []string, p_address []string) {
+/*
+CreateExcelFile Создание файла excel и добавление данных
+*/
+func CreateExcelFile(sName []string, sAddress []string,
+	sComment []string, long []float64,
+	lat []float64, pName []string, pAddress []string) {
 	sname := make(map[string]string)
 	saddress := make(map[string]string)
 	scomment := make(map[string]string)
@@ -299,7 +396,7 @@ func CreateExcelFile(s_name []string, s_address []string,
 	slat := make(map[string]float64)
 	pname := make(map[string]string)
 	paddress := make(map[string]string)
-	for i := 0; i < len(s_name); i++ {
+	for i := 0; i < len(sName); i++ {
 		index := i + 2
 		iName := "A" + strconv.Itoa(index)
 		iAddress := "B" + strconv.Itoa(index)
@@ -308,13 +405,13 @@ func CreateExcelFile(s_name []string, s_address []string,
 		iLat := "E" + strconv.Itoa(index)
 		iPname := "F" + strconv.Itoa(index)
 		iPaddress := "G" + strconv.Itoa(index)
-		sname[iName] = s_name[i]
-		saddress[iAddress] = s_address[i]
-		scomment[iComment] = s_comment[i]
+		sname[iName] = sName[i]
+		saddress[iAddress] = sAddress[i]
+		scomment[iComment] = sComment[i]
 		slon[iLon] = long[i]
 		slat[iLat] = lat[i]
-		pname[iPname] = p_name[i]
-		paddress[iPaddress] = p_address[i]
+		pname[iPname] = pName[i]
+		paddress[iPaddress] = pAddress[i]
 	}
 	f := excelize.NewFile()
 	f.SetCellValue("Sheet1", "A1", "Сеть")
@@ -358,7 +455,7 @@ func CreateExcelFile(s_name []string, s_address []string,
 }
 
 func main() {
-	fileName := "open_test2.kml"
+	fileName := "open.kml"
 	dir, err := os.Getwd()
 	path := dir + string(os.PathSeparator) + fileName
 	if err != nil {
@@ -366,10 +463,11 @@ func main() {
 	}
 
 	var parser KmlParser
-
+	// TODO: Добавить установку дистанции при компилировании
 	p := parser.StartParse(path)
 
 	data := p.ParseFolder()
 
-	CreateDataMap(data.Stores, data.Persons, 10000)
+	CreateDataMap(data.Stores, data.Persons, 1000)
+	fmt.Println("Operation completed successfuly. Excel file was created.")
 }
